01eefa176259dc84281a1fbdb2b13feebf13979d
Minh Vu <minhhv1@uci.edu>
2019-12-31 01:44:12 -0800

    deleted old Navbar, updated README

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "../App.css";
-<<<<<<< HEAD
 import NavbarMain from "./NavbarMain";
 import Navtabs from "./Navtabs";
 
@@ -12,13 +11,6 @@ class App extends Component {
         <Navtabs />
       </React.Fragment>
     );
-=======
-import Navbar from "./Navbar";
-
-class App extends Component {
-  render() {
-    return <Navbar />;
->>>>>>> e694cf81777738f07b3da7def9d706ec6c3bb4c5
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
-=======
-import "bootstrap/dist/css/bootstrap.css";
->>>>>>> e694cf81777738f07b3da7def9d706ec6c3bb4c5
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
