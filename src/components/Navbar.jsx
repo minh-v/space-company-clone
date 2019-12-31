e694cf81777738f07b3da7def9d706ec6c3bb4c5
Minh Vu <minhhv1@uci.edu>
2019-12-30 16:44:20 -0800

    boostrap added, basic navbar up

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="navbar navbar-dark bg-dark navbar-header">
+          <span class="navbar-brand page-title">Space Company</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
